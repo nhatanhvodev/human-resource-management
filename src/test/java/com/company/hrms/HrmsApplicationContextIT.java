778ef8920a54e431cb549f90c4cc8d05b4dd28ed
nhatanhvodev <183174949+nhatanhvodev@users.noreply.github.com>
2026-05-28 14:04:19 +0700

    chore: bootstrap spring boot hrms skeleton

---
+++ src/test/java/com/company/hrms/HrmsApplicationContextIT.java
@@ -0,0 +1,12 @@
+package com.company.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationContextIT {
+
+    @Test
+    void contextLoads() {
+    }
+}
